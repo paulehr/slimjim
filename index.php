e25dd26ff1a751169c0ba5c35c40eca6133d173e
Paul Ehrenreich <paulehr@gmail.com>
2015-05-11 12:58:31 -0400

    adding jquery events and added new post button/field

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ require 'Slim/Slim.php';
 require 'db_functions.php';
 \Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim([
+		'debug' => true
+]);
 
 // Slim routes
 
@@ -18,18 +20,18 @@ $app->get('/', function () use ($app) {
 
 // add a new blog post action
 $app->post('/add', function () use ($app){
-        
+        echo 'This is a Post route';
     }
 );
 
 // update an exsisting blog post
-$app->put('/update/:id', function () use ($app) {
+$app->put('/update/:id', function ($id) use ($app) {
         echo 'This is a PUT route';
     }
 );
 
 // Delete the selected blog post
-$app->delete('/delete/:id', function () use ($app) {
+$app->delete('/delete/:id', function ($id) use ($app) {
         echo 'This is a DELETE route';
     }
 );
--- templates/index.php
+++ templates/index.php
@@ -14,19 +14,36 @@
                 This is a Slim powered blog that is being used a proof of concept, don't replace wordpress with it.
             </p>
             
+            <section id="new_post">
+                <button id="new_post" class="toggle">New Post</button>
+                <form class="hidden">
+                    <input type="text" name="title" value="New Title"><br>
+                    <textarea name="post" rows=10 cols=30>New Post</textarea><br>
+                    <button>Post</button>
+                </form>
+            </section>
             
+            <section id="posts">
             
             <?php
                 foreach ($this->data['articles'] as $post){
-                    echo '<section id='. $post['id'] . '>';
+                    echo '<article id='. $post['id'] . '>';
                     echo '<h2>' . $post['title'] . '</h1>';
                     echo '<p>' . $post['article'] . '</p>';
-                    echo '<p>' . $post['author'] . '</p>';
-                    echo '<a href=/update/' . $post['id'] . 
-                    echo '</section>';
+                    echo '<button class=delete>Delete</button>';
+                    echo '<button class=edit toggle>Edit</button>';
+                    echo '<form id=' . $post['id']  .' '. 'class=hidden>';
+                    echo '<input type=text name=title value="Update Title"><br>';
+                    echo '<textarea name=post rows=10 cols=30>"Update Post"</textarea><br>';
+                    echo '<button class=update>Update</button>';
+                    echo '</form>';
+                    echo '</article>';
                 }
             ?>
            
+            </section>
+            <script type='text/javascript' src="../js/jquery-1.11.3.js"></script>
+            <script type='text/javascript' src="../js/main.js"></script>
         </body>
         
     </html>
